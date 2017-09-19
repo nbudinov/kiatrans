05a558d751f541ba55789a559f75a49867a5e4e8
niki budinov <nbudinov@siriushome.com>
2017-09-19 21:44:43 +0300

    Added BlogPost Admin

---
+++ src/AppBundle/Admin/BlogPostAdmin.php
@@ -0,0 +1,56 @@
+<?php
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class BlogPostAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('Content', array('class' => 'col-md-9'))
+            ->add('title', 'text')
+            ->add('body', 'textarea')
+            ->end()
+
+            ->with('Meta data' , array('class' => 'col-md-3'))
+                ->add('category', 'sonata_type_model', array(
+                    'class' => 'AppBundle\Entity\Category',
+                    'property' => 'name',
+            ))
+            ->end();
+
+//        $formMapper->add('title', 'text')
+//            ->add('body', 'textarea')
+//            ->add('category', 'sonata_type_model', array(
+//                'class' => 'AppBundle\Entity\Category',
+//                'property' => 'name',
+//            ));
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('title')
+            ->add('body')
+            ->add('draft');
+
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('title')
+        ->addIdentifier('body')
+        ->addIdentifier('draft');
+    }
+
+    public function toString($object)
+    {
+        return $object instanceof BlogPost
+            ? $object->getTitle()
+            : 'Blog Post'; // shown in the breadcrumb on the create view
+    }
+
+}
